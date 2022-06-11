b388b684a185c632ad400bbc58f4c33c47efac37
Michael Wong <emporophobe@gmail.com>
2022-06-11 18:41:51 -0400

    Initial commit and example code

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("js") version "1.7.0"
+}
+
+group = "me.empor"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    js(LEGACY) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+}
